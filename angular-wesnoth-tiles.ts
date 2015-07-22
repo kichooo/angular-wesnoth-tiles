f7d3c3ee79160330157868e1b8685d58684357ef
Krzysztof Burliński <krzysztof.burlinski@gmail.com>
2015-07-22 08:03:20 +0200

    Setting up building system.

---
+++ angular-wesnoth-tiles.ts
@@ -0,0 +1,10 @@
+// app.directive('ngSparkline', function() {
+//   return {
+//     restrict: 'A',
+//     template: '<div class="sparkline"></div>'
+//   }
+// });
+
+module WesnothTiles {
+  var dupa = "pies";
+}
--- gulpfile.js
+++ gulpfile.js
@@ -4,33 +4,35 @@ var gulp = require('gulp'),
   concat = require('gulp-concat'),
   notify = require('gulp-notify'),
   copy = require('gulp-copy'),
-  merge = require('merge2');
+  merge = require('merge2'),
+  uglify = require('gulp-uglify'),
+  replace = require('gulp-replace'),
+  mainBowerFiles = require('main-bower-files'),
+  gulpFilter = require('gulp-filter'),
+  util = require('gulp-util'),
+  tsd = require('gulp-tsd');
 
 
-gulp.task('worker', function() {
+gulp.task('lib', function() {
+  var jsFilter = gulpFilter("*.js");
 
-  var tsStreams = gulp.src(['src/worker/**/*.ts'])
-    .pipe(ts({
-      // noImplicitAny: true,
-      noLib: true,
-      declarationFiles: false,
-      target: "ES6",
-      out: 'worker.js'
-    }));
+  var files = mainBowerFiles({
+    includeDev: "inclusive"
+  });
 
-  var jsStream = tsStreams.js
-    .pipe(gulp.dest("bin"))
-
-  return jsStream;
+  return gulp.src(files)
+    .pipe(jsFilter)
+    .pipe(concat("lib.js"))
+    .pipe(gulp.dest("bin/"));
 });
 
-gulp.task('scripts', function() {
-  var tsStreams = gulp.src(['src/main**/*.ts'])
+gulp.task('scripts', ['lib'], function() {
+  var tsStreams = gulp.src(['angular-wesnoth-tiles.ts'])
     .pipe(ts({
       // noImplicitAny: true,
       declarationFiles: true,
       target: "ES6",
-      out: 'wesnoth-tiles.js'
+      out: 'angular-wesnoth-tiles.js'
     }));
 
   var jsStream = tsStreams.js
@@ -53,16 +55,26 @@ gulp.task('scripts', function() {
     }));
 });
 
-gulp.task('app', ['scripts', 'worker'], function() {
-  var streams = gulp.src('test/src/**/*.ts')
+gulp.task('install', function(callback) {
+  tsd({
+    "command": "reinstall", // this plugin supports only "reinstall"
+    "latest": true, // if this property is true, tsd always fetches HEAD definitions
+    "config": "./tsd.json", // file path for configuration file (see below)
+    "opts": {
+      // options, EXPERIMENTAL
+    }
+  }, callback);
+});
+
+gulp.task('app', ['scripts'], function() {
+  var streams = gulp.src('test/testApp.ts')
     .pipe(ts({
       declarationFiles: false,
       target: "ES6",
-      out: 'app.js'
+      out: 'testApp.js'
     }));
 
   var jsStream = streams.js
-    .pipe(concat('app.js'))
     .pipe(gulp.dest("test/bin"));
   return jsStream;
 });
@@ -72,8 +84,8 @@ gulp.task('watch', ['app'], function() {
 });
 
 gulp.task('serve', serve({
-  root: ['test', 'bin', 'tiles'],
-  port: 8001,
+  root: ['test', 'bin', 'bower_components/wesnoth-tiles/tiles'],
+  port: 8002,
 }));
 
-gulp.task("default", ["scripts"]);
+gulp.task("default", ["app"]);
